b09faa1e8a174a0576c4fa368d6a293d5b42438d
Adeeb Asif <adeebasif2005@gmail.com>
2023-09-12 14:28:39 +0400

    promt and print

---
+++ demo.py
@@ -0,0 +1,13 @@
+def prompt_and_print():
+    a = int(input("enter number :"))
+    b = int(input("enter second number: "))
+    add = a + b
+    dif = a - b
+    div = a / b
+    mul = a * b
+    print("a + b = ",add)
+    print("a - b = ", dif)
+    print("a * b = ", mul)
+    print("a / b = ", div)
+
+prompt_and_print()
